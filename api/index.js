86b6a964b3c890a4f48115e273ba9af59887ca8e
Luis Gerardo <gerardwjones@gmail.com>
2020-06-21 22:28:45 -0500

    enable CORS

--- api/index.js
+++ api/index.js
@@ -1,11 +1,14 @@
 const express = require('express')
+const cors = require('cors')
 const bodyParser = require('body-parser')
 const jwt = require('jsonwebtoken')
 const { config } = require('./config')
 
 const app = express()
+const corsOptions = { origin: "http://localhost:5000"}
 
 app.use(bodyParser.json())
+app.use(cors(/* There is where corsOptions belongs */))
 
 app.post("/api/auth/token", function(req, res){
     const { email, username, name} = req.body
@@ -17,4 +20,5 @@ app.post("/api/auth/token", function(req, res){
 const port = 5000
 const server = app.listen(port, function() {
     console.log(`Listening in http://localhost:${server.address().port}`)
+    console.log(`CORS-enabled sweb server listening on port ${port}`)
 })
